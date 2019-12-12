ac11a565d3464765894c6a6dcf255111efdb0f07
Seongjun_Lee_Bit <yuhancam1@gmail.com>
2019-12-12 22:52:20 +0900

    Create

--- app/src/main/java/com/example/bob/ui/ChatList/ChatRoomActivity.java
+++ app/src/main/java/com/example/bob/ui/ChatList/ChatRoomActivity.java
@@ -3,7 +3,6 @@ package com.example.bob.ui.ChatList;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -18,7 +17,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -27,11 +25,9 @@ public class ChatRoomActivity extends AppCompatActivity {
     String User_Name;
     String Room_Name;
 
-    ListView Chat_List, My_Chat_List;
+    ListView Chat_List;
     ArrayList<HashMap<String, String>>list = new ArrayList<HashMap<String, String>>();
-    ArrayList<HashMap<String, String>>my_list = new ArrayList<HashMap<String, String>>();
     SimpleAdapter m_Adapter;
-    SimpleAdapter my_Adapter;
 
     EditText Chat_Enter;
     Button Chat_Send;
@@ -62,79 +58,52 @@ public class ChatRoomActivity extends AppCompatActivity {
                 ChatDTO chat = new ChatDTO(User_Name, Chat_Enter.getText().toString());
                 databaseReference.child(Room_Name).push().setValue(chat);
                 Chat_Enter.setText("");
+
             }
         });
     }
 
     private void addMessage(DataSnapshot dataSnapshot) {
         HashMap<String, String> item = new HashMap<String, String>();
-        HashMap<String, String> my_item = new HashMap<String, String>();
         ChatDTO chatDTO = dataSnapshot.getValue(ChatDTO.class);
 
         if(chatDTO.getUserName().equals(User_Name)) {
-            my_item.put("item1", chatDTO.getUserName());
-            my_item.put("item2", chatDTO.getMessage());
-
-            my_list.add(my_item);
-
-            item.put("item1", "");
-            item.put("item2", "");
+            item.put("item2", chatDTO.getUserName());
+            item.put("item4", chatDTO.getMessage());
 
             list.add(item);
         }
         else {
-            my_item.put("item1", "");
-            my_item.put("item2", "");
-
-            my_list.add(my_item);
-
             item.put("item1", chatDTO.getUserName());
-            item.put("item2", chatDTO.getMessage());
+            item.put("item3", chatDTO.getMessage());
 
             list.add(item);
         }
+        Chat_List.setSelection(m_Adapter.getCount() - 1);
     }
 
     private void removeMessage(DataSnapshot dataSnapshot) {
         HashMap<String, String> item = new HashMap<String, String>();
-        HashMap<String, String> my_item = new HashMap<String, String>();
         ChatDTO chatDTO = dataSnapshot.getValue(ChatDTO.class);
 
         if(chatDTO.getUserName().equals(User_Name)) {
-            my_item.put("item1", chatDTO.getUserName());
-            my_item.put("item2", chatDTO.getMessage());
-
-            my_list.remove(my_item);
-
-            item.put("item1", "");
             item.put("item2", "");
+            item.put("item4", "");
 
             list.remove(item);
         }
         else {
-            my_item.put("item1", "");
-            my_item.put("item2", "");
-
-            my_list.remove(my_item);
-
             item.put("item1", chatDTO.getUserName());
-            item.put("item2", chatDTO.getMessage());
+            item.put("item3", chatDTO.getMessage());
 
             list.remove(item);
         }
     }
 
     private void openChat(String chatName) {
-        // 리스트 어댑터 생성 및 세팅
-
-        m_Adapter = new SimpleAdapter(this, list, R.layout.chat_listview, new String[] {"item1", "item2"}, new int[]{R.id.text1, R.id.text2});
         Chat_List = findViewById(R.id.chat_list);
+        m_Adapter = new SimpleAdapter(this, list, R.layout.chat_listview, new String[] {"item1", "item2", "item3", "item4"}, new int[]{R.id.text1, R.id.text2, R.id.text3, R.id.text4});
         Chat_List.setAdapter(m_Adapter);
-
-        my_Adapter = new SimpleAdapter(this, my_list, R.layout.chat_listview, new String[] {"item1", "item2"}, new int[]{R.id.text1, R.id.text2});
-        My_Chat_List = findViewById(R.id.my_chat_list);
-        My_Chat_List.setAdapter(my_Adapter);
-        //Chat_List.setOnItemClickListener(onListItemClick);
 
         // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
         databaseReference.child(Room_Name).addChildEventListener(new ChildEventListener() {
---
+++ app/src/main/java/com/example/bob/ui/ChatList/ChatRoomSearchActivity.java
@@ -0,0 +1,4 @@
+package com.example.bob.ui.ChatList;
+
+public class ChatRoomSearchActivity {
+}
